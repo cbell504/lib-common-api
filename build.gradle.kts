bae316a5fb14b07ccdea59b97bd709fdea3b651c
Christopher Bell <cbell7@icloud.com>
2024-06-08 12:49:52 -0500

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.christopherbell.dev"
-version = "1.0"
+version = "2024.06.08.01"
 
 dependencies {
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
